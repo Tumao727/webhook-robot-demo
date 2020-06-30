ad1bfc835115e1b2d056a7d293e66c9710c56cda
tumao <tumao@protonmail.com>
2020-06-30 16:36:41 +0800

    feat(test): add json log

--- src/app.service.ts
+++ src/app.service.ts
@@ -17,6 +17,7 @@ export class AppService {
     }
 
     const json = JSON.stringify(testInfo)
+    console.log('json', json)
     return this.httpService.post(
       'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=97967658-62ad-4ee8-9df7-0096e2c60452',
       json
