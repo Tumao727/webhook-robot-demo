5dac7c84918b07de289c1887f4645504c2f53cbf
tumao <tumao@protonmail.com>
2020-06-30 17:38:20 +0800

    feat(test): change service

--- src/app.service.ts
+++ src/app.service.ts
@@ -9,16 +9,16 @@ export class AppService {
   }
 
   sendMsg(param) {
-    const testInfo = {
-      msgtype: 'text',
-      text: {
-        content: `${param.issue.user.login} 提了一个 issue (${param.issue.url}) 给 ${param.issue.assignee.login}`,
-        mentioned_list: ['@all'],
-      },
-    };
+    // const testInfo = {
+    //   msgtype: 'text',
+    //   text: {
+    //     content: `${param.issue.user.login} 提了一个 issue (${param.issue.url}) 给 ${param.issue.assignee.login}`,
+    //     mentioned_list: ['@all'],
+    //   },
+    // };
 
-    const json = JSON.stringify(testInfo)
-    console.log('json', json)
+    // const json = JSON.stringify(testInfo)
+    console.log('param', param)
     // return this.httpService.post(
     //   'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=97967658-62ad-4ee8-9df7-0096e2c60452',
     //   json
