d4d39529619e6bd1f52cdf4329a1e0c730151d51
tumao <tumao@protonmail.com>
2020-06-30 17:49:57 +0800

    feat(test): delete console

--- src/app.service.ts
+++ src/app.service.ts
@@ -22,7 +22,6 @@ export class AppService {
     }
 
     const json = JSON.stringify(testInfo)
-    console.log('param', param)
     return this.httpService.post(
       'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=97967658-62ad-4ee8-9df7-0096e2c60452',
       json
