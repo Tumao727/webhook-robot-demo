c649b4e504ac4981945a4adba85eae0e0466b7fc
tumao <tumao@protonmail.com>
2020-07-01 10:11:30 +0800

    feat(test): update service

--- src/app.service.ts
+++ src/app.service.ts
@@ -9,7 +9,7 @@ export class AppService {
   }
 
   sendMsg(param, type) {
-    let testInfo = {
+    const testInfo = {
       msgtype: 'text',
       text: {
         content: '发送测试消息！！！',
@@ -17,14 +17,30 @@ export class AppService {
       },
     };
 
-    if (type === 'issue') {
-      testInfo.text.content = `${param.issue.user.login} 提了一个 issue (${param.issue.url}) 给 ${param.issue.assignee.login}`
+    if (type === 'issues') {
+      let text = ''
+      switch (param.action) {
+        case 'assigned':
+          text = `${param.issue.user.login} 将 issue (${param.issue.url}) 指给了${param.issue.assignee.login}`;
+          break;
+        case 'closed':
+          text = `${param.issue.user.login} 关闭了 issue (${param.issue.url})`
+          break
+        case 'reopened':
+          text = `${param.issue.user.login} 重新打开了 issue (${param.issue.url})`;
+          break;
+        default:
+          text = `${param.issue.user.login} 提了新 issue (${param.issue.url})`;
+      }
+      testInfo.text.content = text;
+      
     }
 
     const json = JSON.stringify(testInfo)
-    return this.httpService.post(
-      'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=97967658-62ad-4ee8-9df7-0096e2c60452',
-      json
-    );
+    console.log('=== to see json ===', json)
+    // return this.httpService.post(
+    //   'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=97967658-62ad-4ee8-9df7-0096e2c60452',
+    //   json
+    // );
   }
 }
